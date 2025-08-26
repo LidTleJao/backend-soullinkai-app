ca3cd0e46f7183c596487090f3061ac6e08c3623
LidTleJao <praphunpongtke@gmail.com>
2025-08-26 16:32:06 +0700

    fix run server

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
-import  app  from "./src/index";
-// import { db } from "./src/config/firebase";
+// import  app  from "./src/index";
+// // import { db } from "./src/config/firebase";
 
-const port = process.env.port || 5000;
+// const port = process.env.port || 5000;
 
-// // server.listen(port, () =>{
-// //     console.log("Server is Open!!!");
-// // });
+// // // server.listen(port, () =>{
+// // //     console.log("Server is Open!!!");
+// // // });
+// app.listen(port, () => {
+//   console.log(` Server running on http://localhost:${port}`);
+// });
+
+import app from "./src/index";
+
+const port = Number(process.env.PORT) || 5000;
+
 app.listen(port, () => {
-  console.log(` Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
